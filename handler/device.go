c9433343516d3f8ea529499ba87fe2c7d348d76a
Athibet <p_athibet@odds.team>
2019-06-20 14:28:47 +0700

    update upload file image in method POST

--- handler/device.go
+++ handler/device.go
@@ -1,15 +1,29 @@
 package handler
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path"
 
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo"
 	model "gitlab.odds.team/internship/macinodds-api/models"
 	"gopkg.in/mgo.v2/bson"
 )
 
-// Handler
+type (
+	form struct {
+		ID    bson.ObjectId `json:"id" bson:"_id,omitempty"`
+		Name  string        `json:"name" bson:"name"`
+		Email string        `json:"email" bson:"email"`
+		File  string        `json:"file" bson:"file"`
+		Size  int64         `json:"size"`
+	}
+)
 
+// Handler
 func (h *Handler) GetAPI(c echo.Context) (err error) {
 	return c.String(http.StatusOK, "Welcome to Macinodds devices API v.1.0!")
 }
@@ -42,11 +56,35 @@ func (h *Handler) GetByID(c echo.Context) (err error) {
 }
 
 func (h *Handler) CreateDevice(c echo.Context) (err error) {
+	// Source
+	file, err := c.FormFile("img")
+	if err != nil {
+		return err
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// random filename, retaining existing extension.
+	imgName := uuid.Must(uuid.NewV4()).String() + path.Ext(file.Filename)
+	fmt.Println(imgName)
+	filePath := "./devices/" + imgName
+
+	dst, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
 	dv := &model.Device{
-		ID: bson.NewObjectId(),
+		ID:  bson.NewObjectId(),
+		Img: imgName,
 	}
 	if err = c.Bind(dv); err != nil {
-		return
+		return err
 	}
 
 	// Validation
@@ -57,12 +95,17 @@ func (h *Handler) CreateDevice(c echo.Context) (err error) {
 		}
 	}
 
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
 	db := h.DB.Clone()
 	defer db.Close()
 
 	// Save device in database
 	if err = db.DB("macinodds").C("devices").Insert(dv); err != nil {
-		return
+		return err
 	}
 
 	return c.JSON(http.StatusCreated, dv)
