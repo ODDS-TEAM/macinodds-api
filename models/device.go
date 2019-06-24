ba0da583b341e46f9414a3185efe1624c75ce105
Athibet <p_athibet@odds.team>
2019-06-24 16:55:42 +0700

    add structure of Device

--- models/device.go
+++ models/device.go
@@ -1,16 +1,22 @@
 package model
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"time"
 
-// Device structural
+	"gopkg.in/mgo.v2/bson"
+)
+
+// Device holds metadata about a device.
 type (
 	Device struct {
-		ID     bson.ObjectId `json:"_id" bson:"_id,omitempty"`
-		Name   string        `json:"name" bson:"name"`
-		Serial string        `json:"serial" bson:"serial"`
-		Spec   string        `json:"spec" bson:"spec"`
-		Status bool          `json:"status" bson:"status"`
-		Holder string        `json:"holder" bson:"holder"`
-		Img    string        `json:"img" bson:"img"`
+		ID         bson.ObjectId `json:"_id" bson:"_id,omitempty"`
+		Name       string        `json:"name" bson:"name"`
+		Serial     string        `json:"serial" bson:"serial"`
+		Spec       string        `json:"spec" bson:"spec"`
+		Status     bool          `json:"status" bson:"status"`
+		Holder     string        `json:"holder" bson:"holder"`
+		Tel        string        `json:"tel" bson:"tel"`
+		Img        string        `json:"img" bson:"img"`
+		UpdateTime time.Time     `json:"updateTime" bson:"updateTime"`
 	}
 )
