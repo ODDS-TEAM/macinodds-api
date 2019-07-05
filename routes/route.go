62cedfc02f26b9ecfaae2d31572ca84a7a1e8682
Athibet <p_athibet@odds.team>
2019-07-05 15:58:57 +0700

    change path use .env

--- routes/route.go
+++ routes/route.go
@@ -3,14 +3,16 @@ package route
 import (
 	"github.com/labstack/echo"
 	"gitlab.odds.team/internship/macinodds-api/api"
+	"gitlab.odds.team/internship/macinodds-api/config"
 	"gopkg.in/mgo.v2"
 )
 
 // Init initialize api routes and set up a connection.
 func Init(e *echo.Echo) {
+	c := config.Config()
 
 	// Database connection.
-	db, err := mgo.Dial("mac.odds.team:27017")
+	db, err := mgo.Dial(c.DBHost)
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
