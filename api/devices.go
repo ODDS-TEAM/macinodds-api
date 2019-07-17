2c163cf3400181aad6b099aadfd57ab70112b9bb
Athibet <p_athibet@odds.team>
2019-07-17 14:53:29 +0700

    clean step query in func

--- api/devices.go
+++ api/devices.go
@@ -12,7 +12,11 @@ import (
 
 // CreateDevice create a device into the database.
 func (db *MongoDB) CreateDevice(c echo.Context) (err error) {
-	m, err := db.insertDeviceDB(c)
+	file, src, _ := openFile(c)
+	imgName, filePath := genImgID(file.Filename)
+	createFile(filePath, src)
+
+	m, err := db.insertDeviceDB(c, imgName)
 	if err != nil {
 		return err
 	}
@@ -22,48 +26,27 @@ func (db *MongoDB) CreateDevice(c echo.Context) (err error) {
 
 // UpdateDevice updates the entry for a given device.
 func (db *MongoDB) UpdateDevice(c echo.Context) (err error) {
-	id := getID(c)
-	nm := &model.Device{
-		ID:         id,
-		LastUpdate: time.Now(),
-	}
-	if err = c.Bind(nm); err != nil {
-		return
-	}
+	id, newm, m := db.compareDevices(c)
 
-	m := &model.Device{}
-	if err := db.DCol.Find(bson.M{"_id": id}).One(&m); err != nil {
-		return err
-	}
-
-	log.Println("new:", nm.Img)
-	log.Println("old:", m.Img)
-
-	if nm.Img != m.Img || nm.Img == "" {
-		// Source
+	if newm.Img != m.Img || newm.Img == "" {
 		file, src, _ := openFile(c)
-
-		// Random filename, retaining existing extension.
 		imgNewName, filePath := genImgID(file.Filename)
-
 		createFile(filePath, src)
 
-		nm.Img = imgNewName
-
+		newm.Img = imgNewName
 		if m.Img != "" {
 			removeFile(m)
 		}
 	}
+	db.updateDeviceDB(id, newm)
 
-	// Update Mac in database
-	db.updateDeviceDB(id, nm)
-
-	return c.JSON(http.StatusOK, &nm)
+	return c.JSON(http.StatusOK, &newm)
 }
 
 // RemoveDevice removes a given device by its ID.
 func (db *MongoDB) RemoveDevice(c echo.Context) (err error) {
-	db.removeDeviceDB(c)
+	m := db.removeDeviceDB(c)
+	removeFile(m)
 
 	return c.JSON(http.StatusOK, "The device deleted successfully")
 }
@@ -78,14 +61,22 @@ func (db *MongoDB) GetDevices(c echo.Context) (err error) {
 	return c.JSON(http.StatusOK, &m)
 }
 
-func (db *MongoDB) insertDeviceDB(c echo.Context) (*model.Device, error) {
-	file, src, _ := openFile(c)
-	imgName, filePath := genImgID(file.Filename)
-	createFile(filePath, src)
+func (db *MongoDB) GetDevicesByID(c echo.Context) (err error) {
+	return c.JSON(http.StatusCreated, "ok")
+}
 
+func (db *MongoDB) BorrowDevice(c echo.Context) (err error) {
+	return c.JSON(http.StatusCreated, "ok")
+}
+
+func (db *MongoDB) ReturnDevice(c echo.Context) (err error) {
+	return c.JSON(http.StatusCreated, "ok")
+}
+
+func (db *MongoDB) insertDeviceDB(c echo.Context, i string) (*model.Device, error) {
 	m := &model.Device{
 		ID:         bson.NewObjectId(),
-		Img:        imgName,
+		Img:        i,
 		LastUpdate: time.Now(),
 		Borrowing:  false,
 	}
@@ -102,24 +93,26 @@ func (db *MongoDB) insertDeviceDB(c echo.Context) (*model.Device, error) {
 }
 
 func (db *MongoDB) updateDeviceDB(id bson.ObjectId, nm *model.Device) {
-	if err := db.DCol.Update(bson.M{"_id": id}, &nm); err != nil {
+	// Update Mac in database
+	if err := db.DCol.UpdateId(id, &nm); err != nil {
 		return
 	}
 }
 
-func (db *MongoDB) removeDeviceDB(c echo.Context) {
+func (db *MongoDB) removeDeviceDB(c echo.Context) *model.Device {
 	id := getID(c)
 	m := model.Device{}
 
 	if err := db.DCol.Find(bson.M{"_id": id}).One(&m); err != nil {
-		return
+		return nil
 	}
-	removeFile(&m)
 
 	// Remove the device in database
 	if err := db.DCol.RemoveId(id); err != nil {
-		return
+		return nil
 	}
+
+	return &m
 }
 
 func (db *MongoDB) findDevicesDB() ([]*model.Device, error) {
@@ -132,7 +125,35 @@ func (db *MongoDB) findDevicesDB() ([]*model.Device, error) {
 	return m, nil
 }
 
+// find1
+
+func (db *MongoDB) compareDevices(c echo.Context) (bson.ObjectId, *model.Device, *model.Device) {
+	id := getID(c)
+	newm := &model.Device{
+		ID:         id,
+		LastUpdate: time.Now(),
+	}
+	if err := c.Bind(newm); err != nil {
+		return id, nil, nil
+	}
+
+	m := &model.Device{}
+	if err := db.DCol.FindId(id).One(&m); err != nil {
+		return id, newm, nil
+	}
+
+	log.Println("new image:", newm.Img, "| old image:", m.Img)
+
+	return id, newm, m
+}
+
 func getID(c echo.Context) bson.ObjectId {
+	i := c.Param("id")
+	if i == "" {
+		return ""
+	}
+
 	id := bson.ObjectIdHex(c.Param("id"))
+
 	return id
 }
