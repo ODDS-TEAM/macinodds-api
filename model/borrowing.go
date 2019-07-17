0cfec33f4434733cdf339d05b4a5f06826a53a51
Athibet <p_athibet@odds.team>
2019-07-17 15:00:02 +0700

    change data set of borrowing

--- model/borrowing.go
+++ model/borrowing.go
@@ -1,17 +1,26 @@
 package model
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 type (
 	// Borrowing holds metadata about a borrowing event.
 	Borrowing struct {
-		ID         bson.ObjectId `json:"_id"`
-		Date       string        `json:"date" bson:"date"`
+		// ID         bson.ObjectId `json:"_id" bson:"_id"`
+		// Date       time.Time     `json:"date" bson:"date"`
 		Activity   string        `json:"activity" bson:"activity"`
-		UserID     bson.ObjectId `json:"userID bson:"userID"`
 		MacID      bson.ObjectId `json:"macID" bson:"macID"`
-		ReturnDate string        `json:"returnDate" bson:"returnDate"`
+		ReturnDate time.Time     `json:"returnDate" bson:"returnDate"`
 		Memo       string        `json:"memo" bson:"memo"`
 		Location   string        `json:"location" bson:"location"`
+		// Borrower   struct {
+		// 	ID           bson.ObjectId `json:"_id" bson:"_id"`
+		// 	Name         string        `json:"name" bson:"name"`
+		// 	SlackAccount string        `json:"slackAccount" bson:"slackAccount"`
+		// 	Tel          string        `json:"tel" bson:"tel"`
+		// }
 	}
 )
