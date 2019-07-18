57afd035a29ac2d5a8172cff52210f00909837aa
Athibet <p_athibet@odds.team>
2019-07-18 10:17:55 +0700

    add struct of list device

--- model/device.go
+++ model/device.go
@@ -6,8 +6,8 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-// Device holds metadata about a macbook.
 type (
+	// Device holds metadata about a macbook.
 	Device struct {
 		ID         bson.ObjectId `json:"_id" bson:"_id,omitempty"`
 		Name       string        `json:"name" bson:"name"`
@@ -15,7 +15,26 @@ type (
 		Spec       string        `json:"spec" bson:"spec"`
 		Img        string        `json:"img" bson:"img"`
 		Location   string        `json:"location" bson:"location"`
-		LastUpdate time.Time     `json:"lastUpdate" bson:"lastUpdate"`
 		Borrowing  bool          `json:"borrowing" bson:"borrowing"`
+		LastUpdate time.Time     `json:"lastUpdate" bson:"lastUpdate"`
+	}
+
+	DeviceList struct {
+		ID         bson.ObjectId `json:"_id" bson:"_id,omitempty"`
+		Name       string        `json:"name" bson:"name"`
+		Serial     string        `json:"serial" bson:"serial"`
+		Spec       string        `json:"spec" bson:"spec"`
+		Img        string        `json:"img" bson:"img"`
+		Location   string        `json:"location" bson:"location"`
+		ReturnDate time.Time     `json:"returnDate" bson:"returnDate"`
+		Borrowing  bool          `json:"borrowing" bson:"borrowing"`
+		Borrower   Borrower2     `json:"borrower" bson:"borrower"`
+	}
+
+	Borrower2 struct {
+		Name         string `json:"name" bson:"name"`
+		Email        string `json:"email" bson:"email"`
+		SlackAccount string `json:"slackAccount" bson:"slackAccount"`
+		Tel          string `json:"tel" bson:"tel"`
 	}
 )
