4cc583e5530771a41d8cfb642b21ac30baec833d
atb <p_athibet@odds.team>
2019-07-22 17:51:48 +0700

    move function of user case to file user

--- api/auth.go
+++ api/auth.go
@@ -14,31 +14,24 @@ import (
 
 const clientID = "15378607653-f9lfgsml8th6lf50jfq93v3v2f4vpkpr.apps.googleusercontent.com"
 
-type TokenGoogle struct {
-	Token string `json:"token"`
-}
-
+// LogIn using jwt token of Google.
 func (db *MongoDB) LogIn(c echo.Context) (err error) {
-	var login TokenGoogle
+	var login model.TokenGoogle
 	if err := c.Bind(&login); err != nil {
 		return err
 	}
-	// log.Println("token bind", login)
 
-	tokenInfo, err := GetInfo(login.Token)
-	// log.Println("token info", tokenInfo)
+	tokenInfo, err := getInfo(login.Token)
 	if err != nil {
-		// log.Println("token info nil!!")
 		return err
 	}
 
 	firstLogin := false
 	user := db.FindUser(tokenInfo)
 	if user == nil {
-		// first login
-		// log.Println("create new user")
-		firstLogin = true
+		// Create a new user
 		user = db.CreateUser(tokenInfo)
+		firstLogin = true
 	}
 
 	tok, err := genToken(user)
@@ -46,7 +39,7 @@ func (db *MongoDB) LogIn(c echo.Context) (err error) {
 		return err
 	}
 
-	res := &Token{
+	res := &model.Token{
 		Token:      tok,
 		FirstLogin: firstLogin,
 	}
@@ -54,89 +47,7 @@ func (db *MongoDB) LogIn(c echo.Context) (err error) {
 	return c.JSON(http.StatusOK, res)
 }
 
-type Token struct {
-	Token      string `json:"token"`
-	FirstLogin bool   `json:"firstLogin"`
-}
-
-type JwtCustomClaims struct {
-	ID   bson.ObjectId `json:"id"`
-	Role string        `json:"role"`
-	jwt.StandardClaims
-}
-
-func genToken(user *model.User) (string, error) {
-	claims := &JwtCustomClaims{
-		user.ID,
-		user.Role,
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tok, err := token.SignedString([]byte("sMJuczqQPYzocl1s6SLj"))
-	if err != nil {
-		return "", err
-	}
-	return tok, nil
-}
-
-func (db *MongoDB) CreateUser(tokeninfo *oauth2.Tokeninfo) *model.User {
-	u := &model.User{
-		ID:       bson.NewObjectId(),
-		Role:     "individual",
-		Email:    tokeninfo.Email,
-		CreateAt: time.Now(),
-	}
-	if err := db.UCol.Insert(&u); err != nil {
-		return nil
-	}
-
-	return u
-}
-
-func (db *MongoDB) FindUser(tokeninfo *oauth2.Tokeninfo) *model.User {
-	u := &model.User{}
-
-	q := bson.M{
-		"email": tokeninfo.Email,
-	}
-
-	if err := db.UCol.Find(q).One(&u); err != nil {
-		return nil
-	}
-
-	return u
-}
-
-func verifyAudience(aud string) bool {
-	return aud == clientID
-}
-
-func GetInfo(idToken string) (*oauth2.Tokeninfo, error) {
-	oauth2Service, err := oauth2.New(&http.Client{})
-	if err != nil {
-		return nil, err
-	}
-
-	tokenInfoCall := oauth2Service.Tokeninfo()
-	// log.Println("tokenInfoCall:", tokenInfoCall)
-	tokenInfo, err := tokenInfoCall.IdToken(idToken).Do()
-
-	// log.Println("tokenInfo:", tokenInfo.VerifiedEmail)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if !verifyAudience(tokenInfo.Audience) {
-		log.Println("token expire!!")
-		return nil, nil
-	}
-	return tokenInfo, nil
-}
-
+// Register user to Database
 func (db *MongoDB) Register(c echo.Context) (err error) {
 	u := &model.User{}
 	if err := c.Bind(u); err != nil {
@@ -162,20 +73,50 @@ func (db *MongoDB) Register(c echo.Context) (err error) {
 	return c.JSON(http.StatusOK, &u)
 }
 
-func (db *MongoDB) UpdateUser(tokeninfo *oauth2.Tokeninfo) *model.User {
-	u := &model.User{
-		ID:       bson.NewObjectId(),
-		Role:     "individual",
-		Email:    tokeninfo.Email,
-		CreateAt: time.Now(),
-	}
-	if err := db.UCol.Insert(&u); err != nil {
-		return nil
-	}
-
-	return u
-}
-
+// LogOut user and blacklist token
 func (db *MongoDB) LogOut(c echo.Context) (err error) {
 	return c.JSON(http.StatusCreated, "ok")
+}
+
+func verifyAudience(aud string) bool {
+	return aud == clientID
+}
+
+func getInfo(idToken string) (*oauth2.Tokeninfo, error) {
+	oauth2Service, err := oauth2.New(&http.Client{})
+	if err != nil {
+		return nil, err
+	}
+
+	tokenInfoCall := oauth2Service.Tokeninfo()
+	tokenInfo, err := tokenInfoCall.IdToken(idToken).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	if !verifyAudience(tokenInfo.Audience) {
+		log.Println("token expire!!")
+		return nil, nil
+	}
+
+	return tokenInfo, nil
+}
+
+func genToken(user *model.User) (string, error) {
+	claims := &model.JwtCustomClaims{
+		user.ID,
+		user.Role,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tok, err := token.SignedString(
+		[]byte("sMJuczqQPYzocl1s6SLj"),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return tok, nil
 }
