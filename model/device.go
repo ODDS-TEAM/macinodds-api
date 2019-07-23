189bdac77f48b93fd634edc75e032fd552275490
atb <p_athibet@odds.team>
2019-07-23 10:22:09 +0700

    remove deviceslist struct and change borrower struct

--- model/device.go
+++ model/device.go
@@ -9,7 +9,7 @@ import (
 type (
 	// Device holds metadata about a macbook.
 	Device struct {
-		ID         bson.ObjectId `json:"_id" bson:"_id,omitempty"`
+		ID         bson.ObjectId `json:"_id,omitempty" bson:"_id"`
 		Name       string        `json:"name" bson:"name"`
 		Serial     string        `json:"serial" bson:"serial"`
 		Spec       string        `json:"spec" bson:"spec"`
@@ -17,24 +17,16 @@ type (
 		Location   string        `json:"location" bson:"location"`
 		Borrowing  bool          `json:"borrowing" bson:"borrowing"`
 		LastUpdate time.Time     `json:"lastUpdate" bson:"lastUpdate"`
-	}
-
-	DeviceList struct {
-		ID         bson.ObjectId `json:"_id" bson:"_id,omitempty"`
-		Name       string        `json:"name" bson:"name"`
-		Serial     string        `json:"serial" bson:"serial"`
-		Spec       string        `json:"spec" bson:"spec"`
-		Img        string        `json:"img" bson:"img"`
-		Location   string        `json:"location" bson:"location"`
 		ReturnDate time.Time     `json:"returnDate" bson:"returnDate"`
-		Borrowing  bool          `json:"borrowing" bson:"borrowing"`
-		Borrower   Borrower2     `json:"borrower" bson:"borrower"`
+		Borrower   Borrower      `json:"borrower" bson:"borrower"`
 	}
 
-	Borrower2 struct {
-		Name         string `json:"name" bson:"name"`
-		Email        string `json:"email" bson:"email"`
-		SlackAccount string `json:"slackAccount" bson:"slackAccount"`
-		Tel          string `json:"tel" bson:"tel"`
+	// Borrower holds information about a borrower.
+	Borrower struct {
+		ID           bson.ObjectId `json:"_id,omitempty" bson:"_id"`
+		Name         string        `json:"name,omitempty" bson:"name,omitempty"`
+		Email        string        `json:"email,omitempty" bson:"email,omitempty"`
+		SlackAccount string        `json:"slackAccount,omitempty" bson:"slackAccount,omitempty"`
+		Tel          string        `json:"tel,omitempty" bson:"tel,omitempty"`
 	}
 )
