54855aff3d785a6dc0ae653666af023db05c4408
atb <p_athibet@odds.team>
2019-07-23 10:19:04 +0700

    create name strut

--- model/borrowing.go
+++ model/borrowing.go
@@ -9,23 +9,19 @@ import (
 type (
 	// Borrowing holds metadata about a borrowing event.
 	Borrowing struct {
-		ID         bson.ObjectId `json:"_id" bson:"_id"`
+		ID         bson.ObjectId `json:"_id,omitempty" bson:"_id"`
 		Date       time.Time     `json:"date" bson:"date"`
 		Activity   string        `json:"activity" bson:"activity"`
 		ReturnDate time.Time     `json:"returnDate" bson:"returnDate"`
 		Memo       string        `json:"memo" bson:"memo"`
 		Location   string        `json:"location" bson:"location"`
-		Device     DeviceBorrow  `json:"device" bson:"device"`
-		Borrower   Borrower      `json:"borrower" bson:"borrower"`
+		Device     Name          `json:"device" bson:"device"`
+		Borrower   Name          `json:"borrower" bson:"borrower"`
 	}
 
-	DeviceBorrow struct {
-		ID   bson.ObjectId `json:"_id" bson:"_id"`
-		Name string        `json:"name" bson:"name"`
-	}
-
-	Borrower struct {
-		ID   bson.ObjectId `json:"_id" bson:"_id"`
-		Name string        `json:"name" bson:"name"`
+	// Name of device or borrower when query.
+	Name struct {
+		ID   bson.ObjectId `json:"_id,omitempty" bson:"_id"`
+		Name string        `json:"name,omitempty" bson:"name,omitempty"`
 	}
 )
