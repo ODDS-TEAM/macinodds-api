457ac50881d846a5b2ce3a890b1fb95e7e667875
atb <p_athibet@odds.team>
2019-07-25 10:39:51 +0700

    check device and user for borrow device

--- api/borrowings.go
+++ api/borrowings.go
@@ -1,58 +1,68 @@
 package api
 
 import (
-	"log"
 	"net/http"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	model "gitlab.odds.team/internship/macinodds-api/model"
 	"gopkg.in/mgo.v2/bson"
 )
 
+// ReturnDate is
 type ReturnDate struct {
 	ReturnDate string `json:"returnDate" bson:"returnDate"`
 }
 
+// BorrowDevice is
 func (db *MongoDB) BorrowDevice(c echo.Context) (err error) {
+	// edit status device
+	uid := GetIDFromToken(c)
 	id := getID(c)
+
+	// check devices false and not have borrower
+	quid := bson.M{
+		"borrower._id": uid,
+	}
+	qid := bson.M{
+		"_id":       id,
+		"borrowing": false,
+	}
+
+	count, _ := db.DCol.Find(quid).Count()
+	if count > 0 {
+		return c.JSON(http.StatusNotFound, "You can not borrow device more than one unit")
+	}
+	d := &model.Device{}
+	if err = db.DCol.Find(qid).One(d); err != nil {
+		return c.JSON(http.StatusNotFound, "Can not find the device or device not wang")
+	}
+
+	r := &ReturnDate{}
+	if err := c.Bind(r); err != nil {
+		return err
+	}
+	t, err := time.Parse(time.RFC3339, r.ReturnDate)
+	if err != nil {
+		return err
+	}
+
+	// update data device
 	q := bson.M{
 		"_id": id,
 	}
 	ch := bson.M{
 		"$set": bson.M{
 			"borrowing": true,
+			"borrower": bson.M{
+				"_id": uid,
+			},
+			"returnDate": t,
 		},
 	}
-
 	if err := db.DCol.Update(q, &ch); err != nil {
 		return err
 	}
-	m := &model.Device{}
-	if err := db.DCol.FindId(id).One(&m); err != nil {
-		return err
-	}
-
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(jwt.MapClaims)
-	uid := claims["id"].(string)
-	qid := bson.ObjectIdHex(uid)
-
-	u := &model.User{}
-	if err := db.UCol.FindId(qid).One(&u); err != nil {
-		return err
-	}
-
-	log.Println("uid", uid)
-	log.Println(&u)
-
-	var r ReturnDate
-	if err := c.Bind(&r); err != nil {
-		return err
-	}
-	log.Print("Sivaroot R", r)
-	t, err := time.Parse(time.RFC3339, r.ReturnDate)
 
 	b := &model.Borrowing{
 		ID:         bson.NewObjectId(),
@@ -62,10 +72,10 @@ func (db *MongoDB) BorrowDevice(c echo.Context) (err error) {
 		Memo:       "", // <<<
 		Location:   "", // <<<
 		Device: model.Name{
-			ID: m.ID,
+			ID: id,
 		},
 		Borrower: model.Name{
-			ID: u.ID,
+			ID: uid,
 		},
 	}
 
@@ -73,7 +83,7 @@ func (db *MongoDB) BorrowDevice(c echo.Context) (err error) {
 		return err
 	}
 
-	return c.JSON(http.StatusOK, b)
+	return c.JSON(http.StatusOK, "yeh")
 }
 
 func (db *MongoDB) ReturnDevice(c echo.Context) (err error) {
